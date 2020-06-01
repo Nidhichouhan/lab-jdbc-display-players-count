5edce906a6869cbe85b6bc7525b1ab6cfc4ee5af
Nidhi Chouhan <chouhannidhi339@gmail.com>
2020-06-01 23:08:08 +0530

    PROGRAD FSJ0420A111

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,27 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
+		SkillDAO skilldao=new SkillDAO();
+		
+		TreeMap<Integer,Skill> map=new TreeMap<Integer, Skill>();
+		map=skilldao.skillCount();
+		
+		System.out.println("Skills"+ "\t\t" +"Players");
+		for(Entry<Integer, Skill> out:map.entrySet()) {
+			int id=out.getKey();
+			Skill skill=out.getValue();
+			
+			System.out.println(skill.getSkillName()+"\t"+"\t"+ id);
+		}
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,52 @@
+package dao;
 
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public TreeMap<Integer,Skill> skillCount() throws ClassNotFoundException, IOException, SQLException {
+		Connection con=ConnectionManager.getConnection();
+		Statement smt=con.createStatement();
+		ResultSet rs=smt.executeQuery("SELECT * FROM PLAYER2");
+		int countBatting=0;
+		int countBowling=0;
+		int countAllRounder=0;
+		while(rs.next()) {
+			if(rs.getInt(3)==1) {
+				countBatting++;
+			}else if(rs.getInt(3)==2) {
+				countBowling++;
+			}
+			else {
+				countAllRounder++;
+			}
+				
+				
+		}
+		Skill s1=new Skill(1,"BATTING");
+		Skill s2=new Skill(2,"BOWLING");
+		Skill s3=new Skill(3,"ALL ROUNDER");
+		
+		Integer i1=countBatting;
+		Integer i2=countBowling;
+		Integer i3=countAllRounder;
+		
+		TreeMap<Integer, Skill> map=new TreeMap<Integer,Skill>();
+		map.put(i1,s1);
+		map.put(i2,s2);
+		map.put(i3,s3);
+		
+		con.close();
+		
+		return map;
+	}
+}
